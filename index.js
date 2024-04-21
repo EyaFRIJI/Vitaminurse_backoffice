becd68eb13d102a79310d04cb08c2f15eca70fff
EyaFRIJI <126079337+EyaFRIJI@users.noreply.github.com>
2024-04-21 14:53:30 +0200

    2éme configuration du user

--- index.js
+++ index.js
@@ -1,8 +1,53 @@
-const express = require("express")
-const app = express()
-const port = 2000
+const express = require("express");
+const app = express();
+const port = 2000;
+const { connect } = require("mongoose");
+const User = require("./models/user");
 
+connect("mongodb://127.0.0.1:27017/VitamiNurseDB")
+  .then(() => {
+    console.log("connected to db");
+  })
+  .catch((error) => {
+    console.log(error.message);
+  });
 
-app.listen(port,()=>{
-    console.log(`server is running on port ${port}`)
-})
+app.post("/register", (request, response) => {
+  const {
+    nom,
+    prenom,
+    poids,
+    taille,
+    date_naissance,
+    maladies,
+    allergies,
+    email,
+    mot_passe,
+    tel,
+  } = request.body;
+  const user = new User({
+    nom,
+    prenom,
+    poids,
+    taille,
+    date_naissance,
+    maladies,
+    allergies,
+    email,
+    mot_passe,
+    tel,
+  });
+
+  user
+    .save()
+    .then((savedUser) => {
+      response.send(savedUser); //par défaut 200
+    })
+    .catch((error) => {
+      response.status(500).send({ errorMessage: error.message });
+    });
+});
+
+app.listen(port, () => {
+  console.log(`server is running on port ${port}`);
+});
